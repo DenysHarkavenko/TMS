6d4dfb44516b01445f3676f356565e8392c6b410
Harkavenko <garkavenkodenispost@gmail.com>
2024-03-01 22:16:48 +0100

    fix undefined 'projects' routes

--- server/app/controllers/api/projects_controller.rb
+++ server/app/controllers/api/projects_controller.rb
@@ -1,7 +1,51 @@
-class API::ProjectsController < ApplicationController
+class Api::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :update, :destroy]
+
+  # GET /projects
   def index
     @projects = Project.all
-
     render json: @projects, status: :ok
+  end
+
+  # GET /projects/1
+  def show
+    render json: @project, status: :ok
+  end
+
+  # POST /projects
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      render json: @project, status: :created
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /projects/1
+  def update
+    if @project.update(project_params)
+      render json: @project, status: :ok
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /projects/1
+  def destroy
+    @project.destroy
+    head :no_content
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def project_params
+    params.require(:project).permit(:name, :description) #You can add any other parameters you want to allow for the project
   end
 end
