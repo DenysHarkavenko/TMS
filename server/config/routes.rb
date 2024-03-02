7aadc19d84aac9ae0c22cb403a9295295398a22d
Harkavenko <garkavenkodenispost@gmail.com>
2024-03-02 23:57:00 +0100

    add destroy user session

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   namespace :v1 do
     resources :projects do
       resources :tasks
     end
-    resources :sessions, only:[:create, :destroy]
+    resource :sessions, only:[:create, :destroy]
   end
 end
